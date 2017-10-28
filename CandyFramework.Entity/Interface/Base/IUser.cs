488b0dd10b1021c3a10329333335592d6eb562d6
Fatih GÜRDAL <fatican92@gmail.com>
2017-10-29 01:46:08 +0300

    Yarım kalan iş check-in yapmak zorundayım.

---
+++ CandyFramework.Entity/Interface/Base/IUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandyFramework.Entity.Interface.Base
+{
+    public interface IUser
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        public int Id { get; set; }
+        public int MyProperty { get; set; }
+    }
+}

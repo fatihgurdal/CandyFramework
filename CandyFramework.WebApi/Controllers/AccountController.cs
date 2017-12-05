cc4a36622eb56093e99c9c65c6f29821f042336d
Fatih GÜRDAL <fatican92@gmail.com>
2017-12-05 17:37:07 +0300

    api örnek metodları eklendi.

---
+++ CandyFramework.WebApi/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using CandyFramework.BusinessLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CandyFramework.WebApi.Controllers
+{
+    public class AccountController : BaseController
+    {
+        private readonly IUserService _userService;
+        public AccountController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        [HttpGet]
+        public IHttpActionResult Gets()
+        {
+            return Ok(_userService.All());
+        }
+    }
+}
